bea5221ac955a45cf8a7862babdfdbdb11b1e2a8
igorchugurov <igorchugurov@gmail.com>
2024-02-22 17:16:15 +0100

    patch 1.1.18

--- src/components/examples/useTags/Posts.tsx
+++ src/components/examples/useTags/Posts.tsx
@@ -5,9 +5,10 @@ import {
   getRecord,
   updagteOneRecord,
 } from "../../../service/record";
-import { Box } from "@mui/material";
+import { Box, SxProps, Theme } from "@mui/material";
 import TagsList from "../../tags/TagsList";
 import { getOneTag, tagsDataService } from "../../../service/tags";
+import styles from "./Post.module.css";
 
 const Posts = () => {
   const [items, setItems] = useState<IRecodrs[]>([]);
@@ -245,4 +246,60 @@ const ItemWithTag = ({
     </Box>
   );
 };
-const EmptyIcon = () => <span>🚫</span>;
+const EmptyIcon = ({ onClick }: { onClick: (e: any) => void }) => (
+  <Box className={styles.emptyIconTagWrapper} onClick={onClick}>
+    <Icon_shoppingmode onClick={onClick} />
+  </Box>
+);
+interface IconProjectsProps {
+  iconColor?: string;
+  hoverColor?: string;
+  sx?: SxProps<Theme> & { width?: string; height?: string };
+  stroked?: boolean | undefined;
+  onClick?: (e: any) => void;
+  className?: string;
+}
+
+export const Icon_shoppingmode: React.FC<IconProjectsProps> = ({
+  iconColor,
+  hoverColor,
+  sx,
+  onClick,
+  className,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleClick = (e: React.MouseEvent<SVGElement>) => {
+    e.stopPropagation();
+    if (onClick) {
+      onClick(e);
+    }
+  };
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      onClick={handleClick}
+      className={className}
+    >
+      <mask
+        id="mask0_2294_1137"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="12"
+        height="12"
+      >
+        <rect width="12" height="12" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_2294_1137)">
+        <path
+          d="M6.2875 10.7C6.0875 10.9 5.85 11 5.575 11C5.3 11 5.0625 10.9 4.8625 10.7L1.2875 7.125C1.09583 6.93333 1 6.69792 1 6.41875C1 6.13958 1.09583 5.90417 1.2875 5.7125L5.6875 1.3C5.77917 1.20833 5.8875 1.13542 6.0125 1.08125C6.1375 1.02708 6.27083 1 6.4125 1H9.9875C10.2625 1 10.4979 1.09792 10.6938 1.29375C10.8896 1.48958 10.9875 1.725 10.9875 2V5.575C10.9875 5.71667 10.9604 5.85 10.9062 5.975C10.8521 6.1 10.7792 6.20833 10.6875 6.3L6.2875 10.7ZM8.7375 4C8.94583 4 9.12292 3.92708 9.26875 3.78125C9.41458 3.63542 9.4875 3.45833 9.4875 3.25C9.4875 3.04167 9.41458 2.86458 9.26875 2.71875C9.12292 2.57292 8.94583 2.5 8.7375 2.5C8.52917 2.5 8.35208 2.57292 8.20625 2.71875C8.06042 2.86458 7.9875 3.04167 7.9875 3.25C7.9875 3.45833 8.06042 3.63542 8.20625 3.78125C8.35208 3.92708 8.52917 4 8.7375 4ZM5.575 10L9.9875 5.575V2H6.4125L2 6.425L5.575 10Z"
+          fill="#171717"
+        />
+      </g>
+    </svg>
+  );
+};
