f1df3831d7c970132e8f92ff3383eaefa3187f7e
igorchugurov <igorchugurov@gmail.com>
2024-03-07 10:06:52 +0100

    add export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import SqureButton from "./components/buttons/SqureButton";
+import EntityCrudAndSelect from "./components/entityCrudAndSelect/EntityCrudAndSelect";
 import TagsList from "./components/tags/TagsList";
 import { ITag, ITagsDataService } from "./components/tags/model/tag";
 import {
@@ -8,3 +9,4 @@ import {
 export { SqureButton, TagsList };
 export type { ITagsDataService, ITag };
 export { outerHandlersAdd, outerHandlersRemove };
+export { EntityCrudAndSelect };
